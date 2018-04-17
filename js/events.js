f933f935bf174c599a71c403daf4deeda1dc6bfc
mikedorsey1021@gmail.com dorsey <mikedorsey1021@gmail.com>
2018-04-17 18:50:09 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -7,7 +7,7 @@ function getIt (){
 
 function frameIt (){
   $('img').on("load", function (){
-    $('body img').attr('class').addClass('.tasty');
+    $('img').attr('class').addClass('.tasty');
   });
 }
 
